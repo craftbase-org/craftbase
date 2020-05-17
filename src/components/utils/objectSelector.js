e108547dc9d913d88416e41591e873f5e2854327
meetzaveri <meethpatel96@gmail.com>
2020-05-17 22:33:04 +0530

    refactor: refactor and added a new selector tool

---
+++ src/components/utils/objectSelector.js
@@ -0,0 +1,94 @@
+import Two from "two.js";
+
+export default class Selector {
+  constructor(instance, group, x1, x2, y1, y2, showCircle) {
+    this.two = instance;
+    this.group = group;
+    this.area = null;
+    this.circle1 = null;
+    this.circle2 = null;
+    this.circle3 = null;
+    this.circle4 = null;
+    this.circleGroup = null;
+    this.showCircle = showCircle;
+    this.vertices = {
+      x1,
+      x2,
+      y1,
+      y2,
+    };
+  }
+  create() {
+    const { x1, x2, y1, y2 } = this.vertices;
+    console.log("vertices", this.two, x1, x2, y1, y2);
+
+    const area = this.two.makePath(
+      x1,
+      y1,
+      x2,
+      y1,
+
+      x2,
+      y2,
+      x1,
+      y2
+    );
+    area.fill = "rgba(0,0,0,0)";
+    area.opacity = 0;
+    console.log("area", area);
+    this.area = area;
+
+    let circleGroup = null;
+    if (this.showCircle) {
+      const circle1 = this.two.makeCircle(x1, y1, 3);
+      const circle2 = this.two.makeCircle(x2, y1, 3);
+      const circle3 = this.two.makeCircle(x2, y2, 3);
+      const circle4 = this.two.makeCircle(x1, y2, 3);
+      // const circleGroup = this.two.makeGroup(circle1, circle2, circle3, circle4);
+      this.circle1 = circle1;
+      this.circle2 = circle2;
+      this.circle3 = circle3;
+      this.circle4 = circle4;
+      circleGroup = this.two.makeGroup(circle1, circle2, circle3, circle4);
+      circleGroup.opacity = 0;
+      this.circleGroup = circleGroup;
+    }
+
+    const areaGroup = this.two.makeGroup(area, circleGroup);
+    this.group.add(areaGroup);
+    // this.two.udpate();
+  }
+  show() {
+    this.area.opacity = 1;
+    if (this.circleGroup) this.circleGroup.opacity = 1;
+  }
+  hide() {
+    this.area.opacity = 0;
+    if (this.circleGroup) this.circleGroup.opacity = 0;
+  }
+  update(x1, x2, y1, y2) {
+    console.log("on selector update", x1, x2, y1, y2);
+    this.vertices = {
+      x1,
+      x2,
+      y1,
+      y2,
+    };
+    this.area.vertices = [
+      new Two.Anchor(x1, y1, null, null, null, null, Two.Commands.line),
+      new Two.Anchor(x2, y1, null, null, null, null, Two.Commands.line),
+
+      new Two.Anchor(x2, y2, null, null, null, null, Two.Commands.line),
+      new Two.Anchor(x1, y2, null, null, null, null, Two.Commands.line),
+    ];
+    if (this.showCircle) {
+      this.circle1.translation.set(x1, y1);
+      this.circle2.translation.set(x2, y1);
+      this.circle3.translation.set(x2, y2);
+      this.circle4.translation.set(x1, y2);
+      this.circleGroup.opacity = 1;
+    }
+
+    this.area.opacity = 1;
+  }
+}
