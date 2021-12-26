e8059ba7c18265e9f9190490ace5401f4adb5062
meetzaveri <meethpatel96@gmail.com>
2021-12-27 03:37:59 +0530

    tweak: minor tweak

--- src/views/Board/board.js
+++ src/views/Board/board.js
@@ -64,11 +64,11 @@ const BoardViewPage = (props) => {
                     selectCursorMode={selectPanMode}
                     {...props}
                     changeSelectMode={changeSelectMode}
-                    boardData={getBoardData.boardData.components}
+                    boardData={getBoardData?.boardData.components}
                 />
                 <Canvas
                     selectPanMode={selectPanMode}
-                    componentData={getBoardData.boardData.components}
+                    componentData={getBoardData?.boardData.components}
                 />
             </div>
         </>
