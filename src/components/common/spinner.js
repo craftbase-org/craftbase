abee29b118b8b1c361fd4e0ec3d724f0e0e344aa
meetzaveri <meethpatel96@gmail.com>
2022-04-12 16:16:59 +0530

    tweak: minor tweak

--- src/components/common/spinner.js
+++ src/components/common/spinner.js
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import SpinnerWithSize from './spinnerWithSize'
 const Spinner = ({ displayText }) => {
     return (
         <>
-            <div>{displayText ? displayText : 'Loading components ...'}</div>
+            <div>
+                {displayText ? (
+                    displayText
+                ) : (
+                    <SpinnerWithSize
+                        loaderSize="sm"
+                        // customStyles={{
+                        //     margin: 0,
+                        //     marginLeft: '4px',
+                        //     borderBottomColor: '#ABF5D1',
+                        // }}
+                    />
+                )}
+            </div>
         </>
     )
 }
