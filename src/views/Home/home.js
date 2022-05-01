c21ad2f0659007b8e3fd3e25718f65ee03aa8ebc
meetzaveri <meethpatel96@gmail.com>
2022-05-01 17:22:30 +0530

    update: update home page design

--- src/views/Home/home.js
+++ src/views/Home/home.js
@@ -8,10 +8,15 @@ import StickerSVG from 'assets/sticker.svg'
 import TwitterLogoSVG from 'assets/twitter_logo.svg'
 import GithubLogoSVG from 'assets/github_logo.svg'
 import RightArrowWhiteSVG from 'assets/right_arrow_white.svg'
+import CustomizeSVG from 'assets/customize.svg'
+import NoSignupSVG from 'assets/no_signup.svg'
+import BlueStarSVG from 'assets/blue_star.svg'
 
-import ImageContainer from 'components/ProgressiveImageLoader/loader'
-import CraftbaseBoardScreenshotPNG from 'assets/craftbase_board_screenshot.png'
-import CraftbaseBoardScreenshotBlurJPG from 'assets/blur_craftbase_screenshot.jpg'
+// import ImageContainer from 'components/ProgressiveImageLoader/loader'
+// import CraftbaseBoardScreenshotPNG from 'assets/craftbase_board_screenshot.png'
+// import CraftbaseBoardScreenshotBlurJPG from 'assets/blur_craftbase_screenshot.jpg'
+import HomeShapeToolbarJPG from 'assets/home_shape_toolbar.jpg'
+import HomeToolbarBrandingPNG from 'assets/home_toolbar_branding.png'
 import Button from 'components/common/button'
 import ModalContainer from 'components/common/modalContainer'
 import { generateRandomUsernames } from 'utils/misc'
@@ -181,13 +186,14 @@ const HomePage = (props) => {
                 <div
                     className="w-full
                 mt-8
-                h-full px-10 2xl:px-20 py-16 big-desktop:py-40 flex items-center "
+                h-full 
+                px-10 2xl:px-20 py-6 sm:py-16 big-desktop:py-32 
+                flex items-center  flex-wrap"
                 >
-                    <div className="w-5/12 h-full flex items-center justify-center">
+                    <div className="w-full lg:w-5/12 h-full flex items-center justify-center">
                         <div className="text-left transition-opacity duration-300 ease-in-out">
                             <p className="text-base lg:text-4xl 2xl:text-5xl font-bold ">
-                                Brainstorm and prototype with craftbase - an
-                                online whiteboard tool
+                                Create wireframes and design with craftbase.
                             </p>
                             <p className="pt-4 2xl:pt-8 text-sm lg:text-lg 2xl:text-xl ">
                                 Craftbase is an open source whiteboard tool
@@ -196,8 +202,8 @@ const HomePage = (props) => {
                             </p>
                             <p className="pt-4 2xl:pt-8 text-sm lg:text-lg 2xl:text-xl ">
                                 With real-time updates across all devices, you
-                                can share the board with your colleagues by
-                                sending them the board link
+                                can collaborate with your peers in real-time
+                                just by sharing them the board link.
                             </p>
                             <div className="mt-4">
                                 <Button
@@ -228,12 +234,12 @@ const HomePage = (props) => {
                             </div>
                         </div>
                     </div>
-                    <div className="w-7/12 h-full ">
-                        <div className="pl-4 home-page-main-video w-full flex items-center justify-end ">
+                    <div className="w-full lg:w-7/12 pt-8 lg:pt-0 h-full ">
+                        <div className="lg:pl-4 home-page-main-video w-full flex items-center lg:justify-end justify-center ">
                             <iframe
                                 title="vimeo-player"
                                 src="https://player.vimeo.com/video/701362448?h=3b8bc751b0"
-                                className=" w-11/12"
+                                className=" w-full lg:w-11/12"
                                 height="345"
                                 frameborder="0"
                                 allowfullscreen
@@ -252,8 +258,122 @@ const HomePage = (props) => {
                     </div>
                 </div>
 
+                <div className="mt-8">
+                    <div className="w-full flex  flex-wrap">
+                        <div className="w-full sm:w-1/3">
+                            <div>
+                                <img
+                                    src={NoSignupSVG}
+                                    className="w-10 h-10 big-desktop:w-14 big-desktop:h-14 mx-auto"
+                                />
+                            </div>
+                            <div className=" px-8 lg:px-20 big-desktop:text-lg">
+                                <p className="mt-4 text-primary-blue font-bold">
+                                    No sign-up(s) needed
+                                </p>
+                                <p className="mt-2">
+                                    Create as many boards you want without
+                                    registering or signing up on platform.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="w-full sm:w-1/3 pt-8 sm:pt-0">
+                            <div>
+                                <img
+                                    src={BlueStarSVG}
+                                    className="w-10 h-10 big-desktop:w-14 big-desktop:h-14 mx-auto"
+                                />
+                            </div>
+                            <div className=" px-8 lg:px-20 big-desktop:text-lg">
+                                <p className="mt-4 text-primary-blue font-bold">
+                                    Open Source at its core
+                                </p>
+                                <p className="mt-2">
+                                    We beleive in open code. Any one can
+                                    download our source code, tweak and
+                                    customize it for their own needs.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="w-full sm:w-1/3 pt-8 sm:pt-0">
+                            <div>
+                                <img
+                                    src={CustomizeSVG}
+                                    className="w-10 h-10 big-desktop:w-14 big-desktop:h-14 mx-auto"
+                                />
+                            </div>
+                            <div className=" px-8 lg:px-20 big-desktop:text-lg">
+                                <p className="mt-4 text-primary-blue font-bold">
+                                    Customization
+                                </p>
+                                <p className="mt-2">
+                                    We are constantly updating to make it more
+                                    customizable than ever
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="mt-8 px-10 2xl:px-20 py-16 big-desktop:py-40">
+                    <div className="w-full flex items-center ">
+                        <div className="w-1/2">
+                            <div className=" pr-14 ">
+                                <div className="font-bold text-2xl text-left">
+                                    Modify your wireframe elements the way you
+                                    want
+                                </div>
+                                <div className="mt-4">
+                                    <p className="text-left">
+                                        With pre-baked elements, comes the
+                                        customization. You can customize them
+                                        with whatever you need. Whether its
+                                        opacity or border color or text color,
+                                        we have them in our customization bar
+                                    </p>
+                                    <p className="mt-4 text-left">
+                                        Moreover, we have a primary toolbar in
+                                        top which always keeps most used shapes
+                                        and text. So that you can pick them
+                                        without browsing them over
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="branding-imgs" className="w-1/2 h-96">
+                            <div className="w-auto h-full  relative font-bold text-lg ">
+                                <div className="w-auto absolute top-0 left-0 lg:left-5 xl:left-20 shadow-2xl">
+                                    <img
+                                        className="w-full  h-64"
+                                        src={HomeShapeToolbarJPG}
+                                    />
+                                </div>
+                                <div className="w-auto absolute bottom-20 right-0 lg:right-5 xl:right-20 shadow-2xl">
+                                    <img
+                                        className="w-full  h-64"
+                                        src={HomeToolbarBrandingPNG}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* <div className="mt-8">
+                    <div className="font-bold text-2xl">Support and Help</div>
+
+                    <div className="mt-4">
+                        Please contact{' '}
+                        <a href="mailto:meetzaveri96@gmail.com">
+                            meetzaveri96@gmail.com
+                        </a>
+                    </div>
+                </div> */}
+
+                {/* <div className="mt-8 pt-20"> </div> */}
+
                 <footer
-                    className=" fixed bottom-0 
+                    className="  
                     w-full  px-10 2xl:px-20 
                      bg-neutrals-n40  h-20 shadow-inner
                 flex items-center justify-between"
