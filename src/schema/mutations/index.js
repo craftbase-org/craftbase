619abc0fe455fc49879f1b787ac317aea54e3dd5
meetzaveri <meethpatel96@gmail.com>
2026-04-12 09:26:48 +0530

    update: change in queries/mutation schema

--- src/schema/mutations/index.js
+++ src/schema/mutations/index.js
@@ -43,7 +43,9 @@ export const DELETE_COMPONENT_BY_ID = gql`
 `
 
 export const INSERT_BULK_COMPONENTS = gql`
-    mutation MyMutation($objects: [components_component_insert_input!]! = {}) {
+    mutation insertBulkComponents(
+        $objects: [components_component_insert_input!]! = {}
+    ) {
         insert_components_component(objects: $objects) {
             affected_rows
             returning {
@@ -85,7 +87,10 @@ export const DELETE_BULK_COMPONENTS = gql`
 
 export const UPDATE_BOARD_VISIBILITY = gql`
     mutation updateBoardVisibility($id: uuid = "") {
-        update_boards_board_by_pk(pk_columns: { id: $id }, _set: { isPublic: true }) {
+        update_boards_board_by_pk(
+            pk_columns: { id: $id }
+            _set: { isPublic: true }
+        ) {
             id
             isPublic
         }
